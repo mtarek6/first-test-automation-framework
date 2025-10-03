a94a5830b4448c15661517d8eacac81708212fa3
Mostafa Tarek <m.tarek0612@gmail.com>
2025-10-03 16:19:40 +0300

    Fix:fixing download path file separator for linux

--- src/main/java/com/automationexercise/drivers/ChromeFactory.java
+++ src/main/java/com/automationexercise/drivers/ChromeFactory.java
@@ -26,7 +26,14 @@ public class ChromeFactory extends AbstractDriver  {
         options.addArguments("--start-maximized");
         Map<String, Object> prefs = new HashMap<>();
         String userDir = System.getProperty("user.dir");
-        String downloadPath = userDir + "\\src\\test\\resources\\downloads";
+        String downloadPath = userDir + File.separator +
+                "src" +
+                File.separator +
+                "test" +
+                File.separator +
+                "resources" +
+                File.separator +
+                "downloads";
         prefs.put("profile.default_content_settings.popups", 0);
         prefs.put("download.prompt_for_download", false);
         prefs.put("download.default_directory",downloadPath);
